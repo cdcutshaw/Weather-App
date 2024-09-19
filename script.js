5c1b283e692c4cd1047086906ff03cc6661481f9
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-09-19 17:24:56 -0600

    add logic to process and render weather date to the dom

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-
+function handleSubmitSearch() {
+  const searchBar = document.querySelector('.searchBar')
+   searchBar.onsubmit = (event) => {
+     event.preventDefault();
+     const city = document.getElementById('city').value;
+     getWeatherData(city);  
+     searchBar.reset();
+   }
+ }
+ handleSubmitSearch();
 
 async function getWeatherData(city) {
     try {
-      const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=P89F5D9Z6MBHH3YMURCAY5CAW&contentType=json`);
+      const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?iconSet=icons2&unitGroup=us&key=P89F5D9Z6MBHH3YMURCAY5CAW&contentType=json`);
       
       if (!response.ok) {
         throw new Error(`Error: ${response.status} - ${response.statusText}`);
@@ -16,28 +25,46 @@ async function getWeatherData(city) {
   }
 
 function processData (weatherData) {
-   const displayData = {
+   const processsedData = {
+    city : weatherData.address,
     conditions: weatherData.currentConditions.conditions,
-    icon : weatherData.currentConditions.icon,
     temp : weatherData.currentConditions.temp,
-    feelsLike: weatherData.currentConditions.feelslike,
+    icon : weatherData.currentConditions.icon,
     hi : weatherData.days[0].tempmax,
     low : weatherData.days[0].tempmin,
-    precipprob : weatherData.currentConditions.precipprob,
+    feelslike: weatherData.currentConditions.feelslike,
     humidity : weatherData.currentConditions.humidity,
+    precipprob : weatherData.currentConditions.precipprob,
     wind : weatherData.currentConditions.windspeed,
-    uvindex : weatherData.currentConditions.uvindex,
     pressure : weatherData.currentConditions.pressure,
+    uvindex : weatherData.currentConditions.uvindex,
     sunrise : weatherData.currentConditions.sunrise,
     sunset : weatherData.currentConditions.sunset,
     moonphase : weatherData.currentConditions.moonphase,
    }
-   console.log(weatherData);
-   console.log(displayData);
-   return displayData;
-    
+   console.log(weatherData)
+   renderData(processsedData);
 } 
-getWeatherData('knoxville')
+
+
+function renderData (processedData) {
+  document.querySelector('.city').textContent = processedData.city.charAt(0).toUpperCase() + processedData.city.slice(1);
+  document.querySelector('.currentconditions').textContent = `Current Conditions: ${processedData.conditions}`
+  document.querySelector('.temp').textContent = `Current Temp: ${processedData.temp}°F`
+  document.querySelector('.icon').src = `icons/${processedData.icon}.svg`
+  document.querySelector('.hi').textContent = `Hi : ${processedData.hi}°F`
+  document.querySelector('.low').textContent = `Low: ${processedData.low}°F`
+  document.querySelector('.feelslike').textContent = `Feels Like: ${processedData.feelslike}°F`
+  document.querySelector('.humidity').textContent = `Humidity: ${processedData.humidity}%`
+  document.querySelector('.precipprob').textContent = `Chance of Precipitation: ${processedData.precipprob}%`
+  document.querySelector('.wind').textContent = `Wind: ${processedData.wind} mph`
+  document.querySelector('.pressure').textContent = `Pressure: ${processedData.pressure} mbar`
+  document.querySelector('.uvindex').textContent = `UV Index: ${processedData.uvindex} `
+  document.querySelector('.sunrise').textContent = `Sunrise: ${processedData.sunrise} `
+  document.querySelector('.sunset').textContent = `Sunset: ${processedData.sunset} `
+  document.querySelector('.moonphase').textContent = `Moonphase : ${processedData.moonphase} `
+
+} 
 
 
 
