b91a19624455e8ffb71ddffb731f19c6924629ea
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-09-20 16:15:48 -0600

    refine content styling

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
+function loadDefaultData () {
+  getWeatherData('New York City', 'us')
+}
+loadDefaultData()
+
 function handleSubmitSearch() {
   const searchBar = document.querySelector('.searchBar')
    searchBar.onsubmit = (event) => {
      event.preventDefault();
+     const units = getUnits();
      const city = document.getElementById('city').value;
-     getWeatherData(city);  
+     getWeatherData(city, units);  
      searchBar.reset();
+     return city
    }
  }
  handleSubmitSearch();
 
-async function getWeatherData(city) {
+async function getWeatherData(city, units) {
     try {
-      const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?iconSet=icons2&unitGroup=us&key=P89F5D9Z6MBHH3YMURCAY5CAW&contentType=json`);
+      const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?iconSet=icons2&unitGroup=${units}&key=P89F5D9Z6MBHH3YMURCAY5CAW&contentType=json`);
       
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      if (response.status === 400) {
+        throw new Error(` invalid entry: please enter a valid city name`);
       }
   
       const weatherData = await response.json();
@@ -26,7 +33,7 @@ async function getWeatherData(city) {
 
 function processData (weatherData) {
    const processsedData = {
-    city : weatherData.address,
+    city : weatherData.resolvedAddress,
     conditions: weatherData.currentConditions.conditions,
     temp : weatherData.currentConditions.temp,
     icon : weatherData.currentConditions.icon,
@@ -42,49 +49,54 @@ function processData (weatherData) {
     sunset : weatherData.currentConditions.sunset,
     moonphase : weatherData.currentConditions.moonphase,
    }
-   console.log(weatherData)
    renderData(processsedData);
 } 
-
 
 function renderData (processedData) {
   document.querySelector('.city').textContent = processedData.city.charAt(0).toUpperCase() + processedData.city.slice(1);
   document.querySelector('.currentconditions').textContent = `Current Conditions: ${processedData.conditions}`
-  document.querySelector('.temp').textContent = `Current Temp: ${processedData.temp}°F`
   document.querySelector('.icon').src = `icons/${processedData.icon}.svg`
-  document.querySelector('.hi').textContent = `Hi : ${processedData.hi}°F`
-  document.querySelector('.low').textContent = `Low: ${processedData.low}°F`
-  document.querySelector('.feelslike').textContent = `Feels Like: ${processedData.feelslike}°F`
   document.querySelector('.humidity').textContent = `Humidity: ${processedData.humidity}%`
   document.querySelector('.precipprob').textContent = `Chance of Precipitation: ${processedData.precipprob}%`
-  document.querySelector('.wind').textContent = `Wind: ${processedData.wind} mph`
   document.querySelector('.pressure').textContent = `Pressure: ${processedData.pressure} mbar`
   document.querySelector('.uvindex').textContent = `UV Index: ${processedData.uvindex} `
   document.querySelector('.sunrise').textContent = `Sunrise: ${processedData.sunrise} `
   document.querySelector('.sunset').textContent = `Sunset: ${processedData.sunset} `
   document.querySelector('.moonphase').textContent = `Moonphase : ${processedData.moonphase} `
 
+  const units = getUnits()
+  if (units === 'us') {
+    document.querySelector('.temp').textContent = `Current Temp: ${processedData.temp}°F`
+    document.querySelector('.hi').textContent = `Hi : ${processedData.hi}°F`
+    document.querySelector('.low').textContent = `Low: ${processedData.low}°F`
+    document.querySelector('.feelslike').textContent = `Feels Like: ${processedData.feelslike}°F`
+    document.querySelector('.wind').textContent = `Wind: ${processedData.wind} mph `
+  }
+  else if (units === 'metric'){
+    document.querySelector('.temp').textContent = `Current Temp: ${processedData.temp}°C`
+    document.querySelector('.hi').textContent = `Hi : ${processedData.hi}°C`
+    document.querySelector('.low').textContent = `Low: ${processedData.low}°C`
+    document.querySelector('.feelslike').textContent = `Feels Like: ${processedData.feelslike}°C`
+    document.querySelector('.wind').textContent = `Wind: ${processedData.wind} km/h `
+  }
 } 
 
-
-
-function handleDegreeDropdown() {
-    const dropBtn = document.querySelector(".dropBtn");
-    dropBtn.addEventListener("click", function() {
-        document.getElementById("dropList").classList.toggle("show");
-    });
-
-    window.onclick = function(event) {
-        if(!event.target.matches('.dropBtn')) {
-            let dropdowns = document.getElementsByClassName("dropContent");
-            let i;
-            for (i = 0; i < dropdowns.length; i++) {
-                let openDropdown = dropdowns[i];
-                if(openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }
+function handleUnitDropdown() {
+  const unitSelector = document.getElementById('units');
+  unitSelector.addEventListener("change", () => {
+    const units = getUnits();
+    const city = document.querySelector('.city').textContent;
+    getWeatherData(city, units)
+  } )
 }
-handleDegreeDropdown();
+handleUnitDropdown()
+
+function getUnits () {
+  const unitval = document.getElementById('units').value;
+  const unitvalLower = unitval.toLowerCase()
+  return (unitvalLower)
+}
+
+
+
+
